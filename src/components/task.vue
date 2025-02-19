f9764281a70fb9edb8df6e87e57303d2f56d2af3
NgoVanLoc <62205ngovanloc@gmail.com>
2025-02-19 20:42:25 +0700

    first commit

---
+++ src/components/task.vue
@@ -0,0 +1,103 @@
+<script setup>
+// defineProps({
+//   msg: {
+//     type: String,
+//     required: false,
+//   },
+// })
+</script>
+
+<template>
+  <div class="container">
+    <div class="content">
+      <div class="header">
+        <a href="" class="subject">Marketing</a>
+      </div>
+      <h2 class="title">Write SEO article for new product</h2>
+      <div class="break"></div>
+      <p class="description">
+        This is an existential moment for effective altruism and the rationalist
+        community writ-large
+      </p>
+      <div class="time">
+        <i class="ti-alarm-clock"></i>
+        <div class="create-time">June 30,2022</div>
+      </div>
+    </div>
+    <div class="btn-header">
+      <button class="ti-eraser"></button>
+      <button class="ti-trash"></button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+template {
+    
+}
+.container {
+  display: flex;
+  justify-content: space-between;
+  align-items: start;
+  width: 100%;
+  /* height: 50px; */
+  padding: 20px 30px;
+  background-color: #fff;
+  border: 1px solid #706d6d;
+  border-radius: 8px;
+}
+.content {
+  width: 70%;
+  height: 150px;
+}
+
+.header {
+}
+
+.subject {
+  color: #4d76e4;
+}
+
+.btn-header {
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  margin-top: 10px;
+}
+
+.btn-header button {
+  border: none;
+  background-color: transparent;
+  color: #000;
+  font-size: 24px;
+  cursor: pointer;
+}
+.btn-header button:hover {
+    color: #fff;
+    background: #928d8d;
+    border-radius: 5px;
+}
+
+.title {
+  color: #000;
+  font-size: 1rem;
+  font-weight: bold;
+}
+
+.break {
+  margin-bottom: 20px;
+  border-bottom: 2px solid #ccc;
+}
+
+.description {
+  font-size: 0.8rem;
+  margin-top: -10px;
+  color: #666;
+}
+
+.time {
+  color: #666;
+  display: flex;
+}
+</style>
